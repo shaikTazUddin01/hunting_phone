20295da4e63f5625d4bc6acd8ddbaee4035668fc
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-26 14:14:33 +0600

    show phone data

--- phone.js
+++ phone.js
@@ -1,8 +1,35 @@
-const loadphone=async()=>{
-    const res=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    const data=await res.json()
-    const phone=data.data
-    console.log(phone)
+const loadphone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json()
+    const phone = data.data
+
+    displayPhones(phone)
+}
+
+const displayPhones = (phones) => {
+
+    const phoneContain = document.getElementById('phone-contain')
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = "card bg-gray-200 shadow-xl p-5"
+
+        phoneCard.innerHTML = `
+        <figure className="px-10 pt-10">
+                <img src=${phone?.image} alt="Shoes"
+                    className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        `
+        phoneContain.appendChild(phoneCard)
+    });
+
 }
 
 loadphone()
