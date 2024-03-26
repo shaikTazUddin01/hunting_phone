09044c83313746da364c49fa281093537f4bd651
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-26 14:30:49 +0600

    added simple search funtion

--- phone.js
+++ phone.js
@@ -1,5 +1,5 @@
-const loadphone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadphone = async (searchValue) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
     const data = await res.json()
     const phone = data.data
 
@@ -9,6 +9,7 @@ const loadphone = async () => {
 const displayPhones = (phones) => {
 
     const phoneContain = document.getElementById('phone-contain')
+    phoneContain.textContent = ""
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
@@ -20,7 +21,7 @@ const displayPhones = (phones) => {
                     className="rounded-xl" />
             </figure>
             <div className="card-body items-center text-center">
-                <h2 className="card-title">Shoes!</h2>
+                <h2 className="card-title">${phone?.phone_name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div className="card-actions">
                     <button className="btn btn-primary">Buy Now</button>
@@ -32,4 +33,9 @@ const displayPhones = (phones) => {
 
 }
 
-loadphone()
+const handleseach = () => {
+    const searchField = document.getElementById('searchField')
+    const searchValue = searchField.value
+    console.log("search")
+    loadphone(searchValue)
+}
